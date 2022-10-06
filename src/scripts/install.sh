3c7fd1047aafa21faaaacd720df26088b07a4ddc
David Buzinski <davidb@mathworks.com>
2022-10-06 14:41:35 -0400

    remove all the junk in logs and resolve latest

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -24,12 +24,12 @@ if [[ $os = Linux ]]; then
     apt-get clean && apt-get autoremove
 fi
 
-# install matlab-batch
+# set os specific options
 if [[ $os = CYGWIN* || $os = MINGW* || $os = MSYS* ]]; then
     batchInstallDir='/c/Program Files/matlab-batch'
     rootdir=$(cygpath "$rootdir")
     mpmurl="$mpmbaseurl/win64/mpm";
-    mpmSetup="unzip -q $tmpdir/mpm -d $tmpdir"
+    mpmsetup="unzip -q $tmpdir/mpm -d $tmpdir"
     mpmPath="$tmpdir/bin/win64/mpm"
 else
     batchInstallDir='/opt/matlab-batch'
@@ -37,20 +37,25 @@ else
     mpmPath="$tmpdir/mpm"
 fi
 
+if [[ $PARAM_RELEASE = "latest" ]]; then
+    version=$(curl https://mw-ci-static-dev.s3.amazonaws.com/matlab-deps/v0/versions.json | jq .latest)
+else
+    version=${PARAM_RELEASE}
+fi
+
 # install mpm
 curl -o "$tmpdir/mpm" -sfL $mpmurl
-ls $tmpdir
-eval $mpmSetup
-ls $tmpdir
+eval $mpmsetup
 chmod +x "$mpmPath"
 mkdir rootdir
 
 "$mpmPath" install \
-    --release="${PARAM_RELEASE}" \
+    --release="$version" \
     --destination="$rootdir" \
     --products ${PARAM_PRODUCTS} MATLAB Parallel_Computing_Toolbox
 
 
+# install matlab-batch
 downloadAndRun https://ssd.mathworks.com/supportfiles/ci/matlab-batch/v0/install.sh "$batchInstallDir"
 
 # add MATLAB and matlab-batch to path
