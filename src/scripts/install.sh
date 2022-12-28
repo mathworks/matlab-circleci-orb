fc6c70e5bce9c6e3b33f41228667c72626a01935
David Buzinski <103441853+davidbuzinski@users.noreply.github.com>
2022-12-28 08:03:25 -0500

    Mpm init (#44)
    * initial test
    
    * fixing shellcheck finding
    
    * fixing shellcheck finding again
    
    * fixing params
    
    * no wget
    
    * updating to figure out what's going on with install
    
    * fixing typo
    
    * updating curl  command
    
    * fixin some path issues for windows and linux both
    
    * remove all the junk in logs and resolve latest
    
    * trying to update formatting? r2022b is a valid release...
    
    * fixing quotes around version problem
    
    * removing macos because no mpm support yet
    
    * adding simulink to fix failing test
    
    * SL test dependency?
    
    * down the dependency rabbithole...
    
    * Update install.sh
    
    * Update test-deploy.yml
    
    testing that it exits non-zero if mpm fails
    
    * forgot to add new job to workflow
    
    * testing error flags recommended by circle
    
    * testing using powershell for windows
    
    * fixing quotes
    
    * fixin more quotes
    
    * Update install.sh
    
    * reverting changes
    
    * Fixing unset variable on linux
    
    * Update install.sh
    
    * testing across more releases
    
    * Update test-deploy.yml
    
    * Update test-deploy.yml
    
    * doing some post-install debugging
    
    * trying some more windows debuggin
    
    * Update install.sh
    
    * Update install.sh
    
    * Update test-deploy.yml
    
    * Update install.sh
    
    * Update test-deploy.yml
    
    * add no-output-timeout and clean up a bit
    
    * fixing broken install script
    
    * Update install.sh
    
    * Update install.sh
    
    * trying remove Simulink_Coverage and Simulink_Test
    
    * Update test-deploy.yml
    
    * Update test-deploy.yml
    
    * Update test-deploy.yml
    
    * Update test-deploy.yml
    
    * Update test-deploy.yml
    
    * update variable names. add sudoIfAvailable
    
    * set bash opts outside of sudoIfAvailable
    
    * Case insensitive release check and wrap release in quotes
    
    * remove update version from release in setupdeps script
    
    * debugging because local works differently from CI
    
    * trying to fix grep in release
    
    * fixing shellcheck finding
    
    * trying different pattern for grep
    
    * testing without -o in grep
    
    * debug echos :(
    
    * forgot to allow latest for release
    
    * Apply suggestions from code review
    
    Co-authored-by: Mark Cafaro <34887852+mcafaro@users.noreply.github.com>
    
    * cleaning up os specific vars
    
    * Update release info (#45)
    
    * using latest.txt
    
    * Update install.sh
    
    * Update install.sh
    
    * Update install.sh
    
    Co-authored-by: Mark Cafaro <34887852+mcafaro@users.noreply.github.com>

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,45 +1,82 @@
-downloadAndRun() {
-    url=$1
-    shift
+#!/usr/bin/env bash
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+sudoIfAvailable() {
     if [[ -x $(command -v sudo) ]]; then
-    curl -sfL $url | sudo -E bash -s -- "$@"
+    sudo -E bash "$@"
     else
-    curl -sfL $url | bash -s -- "$@"
+    bash "$@"
     fi
 }
 
+downloadAndRun() {
+    url=$1
+    shift
+    curl -sfL $url | sudoIfAvailable -s -- "$@"
+}
+
 os=$(uname)
-
-# installer does not support the Docker executor type on Linux
-if [[ $os = Linux ]] && awk -F/ '$2 == "docker"' /proc/self/cgroup | read -r; then
-    echo 'The Docker executor type is not supported.'
-    exit 1
-fi
-
-# install core system dependencies
-if [[ $os = Linux ]]; then
-    downloadAndRun https://ssd.mathworks.com/supportfiles/ci/matlab-deps/v0/install.sh "${PARAM_RELEASE}"
-fi
-
-# install ephemeral version of MATLAB
-if [ -n "${MATHWORKS_ACCOUNT}" ] &&  [ -n "${MATHWORKS_TOKEN}" ]; then
-    activationFlag="--skip-activation"
-fi
-
-downloadAndRun https://ssd.mathworks.com/supportfiles/ci/ephemeral-matlab/v0/ci-install.sh --release "${PARAM_RELEASE}" $activationFlag
-
 tmpdir=$(dirname "$(mktemp -u)")
-rootdir=$(cat "$tmpdir/ephemeral_matlab_root")
+rootdir="$tmpdir/matlab_root"
+mpmbaseurl="https://www.mathworks.com/mpm"
+
+# resolve release
+if [[ ${PARAM_RELEASE,,} = "latest" ]]; then
+    mpmrelease=$(curl https://ssd.mathworks.com/supportfiles/ci/matlab-release/v0/latest)
+else
+    mpmrelease="${PARAM_RELEASE}"
+fi
+
+# install system dependencies
+if [[ $os = Linux ]]; then
+    # install MATLAB dependencies
+    release=$(echo "${mpmrelease}" | grep -ioE "(r[0-9]{4}[a-b])")
+    downloadAndRun https://ssd.mathworks.com/supportfiles/ci/matlab-deps/v0/install.sh "$release"
+    # install mpm depencencies
+    sudoIfAvailable -c "apt-get install --no-install-recommends --no-upgrade --yes \
+        wget \
+        unzip \
+        ca-certificates"
+fi
+
+# set os specific options
+if [[ $os = CYGWIN* || $os = MINGW* || $os = MSYS* ]]; then
+    batchinstalldir='/c/Program Files/matlab-batch'
+    mpmpath="$tmpdir/bin/win64/mpm"
+    mpmsetup="unzip -q $tmpdir/mpm -d $tmpdir"
+    mwarch="win64"
+
+    rootdir=$(cygpath "$rootdir")
+    mpmpath=$(cygpath "$mpmpath")
+else
+    batchinstalldir='/opt/matlab-batch'
+    mpmpath="$tmpdir/mpm"
+    mpmsetup=""
+    mwarch="glnxa64"
+fi
+
+# install mpm
+curl -o "$tmpdir/mpm" -sfL "$mpmbaseurl/$mwarch/mpm"
+eval $mpmsetup
+chmod +x "$mpmpath"
+mkdir -p "$rootdir"
+
+# install matlab
+"$mpmpath" install \
+    --release=$mpmrelease \
+    --destination="$rootdir" \
+    --products ${PARAM_PRODUCTS} MATLAB Parallel_Computing_Toolbox
 
 # install matlab-batch
-if [[ $os = CYGWIN* || $os = MINGW* || $os = MSYS* ]]; then
-    batchInstallDir='/c/Program Files/matlab-batch'
-    rootdir=$(cygpath "$rootdir")
-else
-    batchInstallDir='/opt/matlab-batch'
-fi
-
-downloadAndRun https://ssd.mathworks.com/supportfiles/ci/matlab-batch/v0/install.sh "$batchInstallDir"
+downloadAndRun https://ssd.mathworks.com/supportfiles/ci/matlab-batch/v0/install.sh "$batchinstalldir"
 
 # add MATLAB and matlab-batch to path
-echo 'export PATH="'$rootdir'/bin:'$batchInstallDir':$PATH"' >> $BASH_ENV
+echo 'export PATH="'$rootdir'/bin:'$batchinstalldir':$PATH"' >> $BASH_ENV
