8fde6811d60a350b1f0a13d664fb84f7cc25577d
David Buzinski <davidb@mathworks.com>
2024-01-25 08:59:09 -0500

    fix cruft

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -93,7 +93,3 @@ chmod +x "$batchdir/matlab-batch$binext"
 
 # add MATLAB and matlab-batch to path
 echo 'export PATH="'$rootdir'/bin:'$batchdir':$PATH"' >> $BASH_ENV
-<<<<<<< HEAD
-=======
-
->>>>>>> 1fb0d30 (update to main)
