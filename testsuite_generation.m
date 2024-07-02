060aa26f6b1c307da07784c7e55f462fa29947a0
vanditaMW <173173349+vanditaMW@users.noreply.github.com>
2024-07-02 11:18:30 +0530

    poc3

--- testsuite_generation.m
+++ testsuite_generation.m
@@ -1,17 +1,18 @@
 function suite = testsuite_generation(stdout, tempFile)
-
     import matlab.unittest.TestSuite;
     stderr = fileread(tempFile);
     disp(stderr);
-    
-    stdout = strtrim(strsplit(stdout, '\n'));
+
+    stdout = strsplit(stdout, '\n');
     stdout = stdout(~cellfun('isempty', stdout));
-    
+    stdout = strtrim(stdout);
+    stdoutCellArray = stdout(:)';
+
     suites = {};
     cd tests
-    for i = 1:length(stdout)
-    testFilePath = fullfile('.', [stdout{i}, '.m']);
-    suites{end+1} = TestSuite.fromFile(testFilePath);
+    for i = 1:length(stdoutCellArray)
+        testFilePath = fullfile('.', [stdoutCellArray{i}, '.m']);
+        suites{end+1} = TestSuite.fromFile(testFilePath);
     end
     suite = [suites{:}];
 end
