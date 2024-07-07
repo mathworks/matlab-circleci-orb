97ac3a2bf88a99d00ffca9b617815313dd5d6f0a
vanditaMW <173173349+vanditaMW@users.noreply.github.com>
2024-07-08 01:02:58 +0530

    store_artifacts

--- sample.m
+++ sample.m
@@ -1,6 +1,8 @@
+
 import matlab.unittest.TestRunner;
 import matlab.unittest.plugins.XMLPlugin;
 import matlab.unittest.plugins.TestReportPlugin;
+
 
 suite = testsuite(pwd, 'IncludingSubfolders', true);
 testFiles = unique({suite.TestParentName});
@@ -10,15 +12,17 @@ tempSplitFile = tempname;
 fid = fopen(tempAllFile, 'w');
 fprintf(fid, '%s\n', testFiles{:});
 fclose(fid);
-command = sprintf('circleci tests split --split-by=timings %s 2> %s', tempAllFile, tempSplitFile); 
-[status, stdout] = system(command);
-suite = testsuite_generation(stdout, tempSplitFile);
+% command = sprintf('circleci tests split --split-by=timings %s 2> %s', tempAllFile, tempSplitFile);
+% [status, stdout] = system(command);
+% suite = testsuite_generation(stdout, tempSplitFile);
 delete(tempAllFile);
 delete(tempSplitFile);
 
+
 runner = TestRunner.withTextOutput();
-runner.addPlugin(TestReportPlugin.producingPDF('tests/results.pdf'));
-runner.addPlugin(XMLPlugin.producingJUnitFormat('tests/results.xml'));
+runner.addPlugin(TestReportPlugin.producingPDF('results.pdf'));
+runner.addPlugin(XMLPlugin.producingJUnitFormat('results.xml'));
+
 
 results = runner.run(suite);
 display(results);
