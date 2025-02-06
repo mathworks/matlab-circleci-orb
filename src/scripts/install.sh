02c4da3b0f85e0789c311fd7e31b2c50e8f88c1a
David Buzinski <davidb@mathworks.com>
2025-02-06 13:00:21 -0500

    refix

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -67,6 +67,7 @@ batchdir="$tmpdir/matlab-batch"
 mpmdir="$tmpdir/mpm"
 batchbaseurl="https://ssd.mathworks.com/supportfiles/ci/matlab-batch/v1"
 mpmbaseurl="https://www.mathworks.com/mpm"
+releasestatus=""
 
 # resolve release
 parsedrelease=$(echo "$PARAM_RELEASE" | tr '[:upper:]' '[:lower:]')
@@ -74,7 +75,7 @@ if [[ "$parsedrelease" = "latest" ]]; then
     mpmrelease=$(stream https://ssd.mathworks.com/supportfiles/ci/matlab-release/v0/latest)
 elif [[ "$parsedrelease" = "latest-including-prerelease" ]]; then
     mpmrelease=$(stream https://ssd.mathworks.com/supportfiles/ci/matlab-release/v0/latest-including-prerelease)
-    release_status_flag="--release-status=Prerelease"
+    releasestatus="--release-status=Prerelease"
 else
     mpmrelease="$parsedrelease"
 fi
@@ -142,7 +143,7 @@ chmod +x "$batchdir/matlab-batch$binext"
 "$mpmdir/mpm$binext" install \
     --release="$mpmrelease" \
     --destination="$rootdir" \
-    ${release_status_flag} \
+    ${releasestatus} \
     --products ${PARAM_PRODUCTS} MATLAB
 
 # add MATLAB and matlab-batch to path
