b8f8c565f1988310c2cee2d23eed3a79eac85c9b
David Buzinski <davidb@d13-dev.dhcp.mathworks.com>
2026-08-27 08:39:36 -0400

    update latest-including-prerelease

--- src/scripts/prepare-install-metadata.sh
+++ src/scripts/prepare-install-metadata.sh
@@ -4,18 +4,11 @@ mkdir -p ~/.matlab-circleci-orb
 eval "$UTILS"
 
 mpmrelease=""
-releasestatus=""
 parsedrelease=$(echo "$PARAM_RELEASE" | tr '[:upper:]' '[:lower:]')
 if [[ "$parsedrelease" = "latest" ]]; then
     mpmrelease=$(stream https://ssd.mathworks.com/supportfiles/ci/matlab-release/v0/latest)
 elif [[ "$parsedrelease" = "latest-including-prerelease" ]]; then
-    fetched=$(stream https://ssd.mathworks.com/supportfiles/ci/matlab-release/v0/latest-including-prerelease)
-    if [[ "$fetched" == *prerelease ]]; then
-        mpmrelease="${fetched%prerelease}"
-        releasestatus="--release-status=Prerelease"
-    else
-        mpmrelease="$fetched"
-    fi
+    mpmrelease=$(stream https://ssd.mathworks.com/supportfiles/ci/matlab-release/v0/latest-including-prerelease)
 else
     mpmrelease="$parsedrelease"
 fi
@@ -26,6 +19,5 @@ if [[ "$mpmrelease" < "r2020b" ]]; then
 fi
 
 echo "export RELEASE=\"$mpmrelease\"" > ~/.matlab-circleci-orb/install-metadata.sh
-echo "export RELEASE_STATUS=\"$releasestatus\"" >> ~/.matlab-circleci-orb/install-metadata.sh
 SORTED_PRODUCTS=$(echo "$PARAM_PRODUCTS" | tr ' ' '\n' | sort | xargs)
 echo "export PRODUCTS=\"$SORTED_PRODUCTS\"" >> ~/.matlab-circleci-orb/install-metadata.sh
